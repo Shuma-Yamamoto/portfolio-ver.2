639271a6b0edb1b41a251cef1c30e5d6a8be165a
Shuma-Yamamoto <s.yamamoto.eng@gmail.com>
2024-01-24 16:31:19 +0900

    トップ画面の作成

--- src/top.js
+++ src/top.js
@@ -12,12 +12,6 @@ export default function Top() {
           <div className="kanji">脩&nbsp;眞</div>
           <div className="roman">shuma.</div>
           <div className="birthday">(327)</div>
-          {/* <div class="scrollContainer">
-            <div class="chevron"></div>
-            <div class="chevron"></div>
-            <div class="chevron"></div> */}
-            {/* <span class="scrollText">Scroll down</span> */}
-          {/* </div> */}
           <svg class="arrows">
             <path class="a1" d="M0 0 L30 32 L60 0"></path>
             <path class="a2" d="M0 20 L30 52 L60 20"></path>
